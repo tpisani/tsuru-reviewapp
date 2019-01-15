c2311dcf6c1f0ab04bd21857e48f27de9a9a9a44
Diego Silva de Salles <dsalles@corp.globo.com>
2019-01-15 16:52:50 -0200

    correcao teste drop command

--- config.go
+++ config.go
@@ -46,9 +46,38 @@ func ConfigTsuru() ReviewAppConfig {
 		os.Exit(1)
 	}
 
-	fmt.Println(token, target)
-
 	f, err := os.Open("./tsuru-reviewapp.yml")
+	if err != nil {
+		fmt.Println("no tsuru-reviewapp.yml")
+		os.Exit(1)
+	}
+
+	var config ReviewAppConfig
+	err = yaml.NewDecoder(f).Decode(&config)
+	if err != nil {
+		fmt.Println("unable to parse config file")
+		os.Exit(1)
+	}
+	f.Close()
+
+	return config
+}
+
+func ConfigTsuruTest() ReviewAppConfig {
+
+	token := os.Getenv("TSURU_TOKEN")
+	if token == "" {
+		fmt.Println("missing Tsuru token")
+		os.Exit(1)
+	}
+
+	target := os.Getenv("TSURU_TARGET")
+	if target == "" {
+		fmt.Println("missing Tsuru target")
+		os.Exit(1)
+	}
+
+	f, err := os.Open("../tsuru-reviewapp.yml")
 	if err != nil {
 		fmt.Println("no tsuru-reviewapp.yml")
 		os.Exit(1)
